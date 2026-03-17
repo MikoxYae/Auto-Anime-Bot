402b36b2150caa8c01c79056c419b165a7db9aff
Yae Miko <aloksinghkumar756@gmail.com>
2026-03-17 08:02:03 +0530

    Update cmds.py

--- bot/modules/cmds.py
+++ bot/modules/cmds.py
@@ -1,5 +1,6 @@
-from asyncio import sleep as asleep, gather
-from pyrogram.filters import command, private, user
+from asyncio import sleep as asleep
+from urllib.parse import unquote, parse_qs, urlparse
+from pyrogram.filters import command, private, user, document
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import FloodWait, MessageNotModified
 
@@ -7,7 +8,10 @@ from bot import bot, bot_loop, Var, ani_cache
 from bot.core.database import db
 from bot.core.func_utils import decode, is_fsubbed, get_fsubs, editMessage, sendMessage, new_task, convertTime, getfeed
 from bot.core.auto_animes import get_animes
+from bot.core.tordownload import TorDownloader
 from bot.core.reporter import rep
+
+pending_torrent = {}
 
 @bot.on_message(command('start') & private)
 @new_task
@@ -31,13 +35,15 @@ async def start_msg(client, message):
                 btns[-1].insert(1, InlineKeyboardButton(bt, url=link))
             else:
                 btns.append([InlineKeyboardButton(bt, url=link)])
-        smsg = Var.START_MSG.format(first_name=from_user.first_name,
-                                    last_name=from_user.first_name,
-                                    mention=from_user.mention, 
-                                    user_id=from_user.id)
+        smsg = Var.START_MSG.format(
+            first_name=from_user.first_name,
+            last_name=from_user.first_name,
+            mention=from_user.mention,
+            user_id=from_user.id
+        )
         if Var.START_PHOTO:
             await message.reply_photo(
-                photo=Var.START_PHOTO, 
+                photo=Var.START_PHOTO,
                 caption=smsg,
                 reply_markup=InlineKeyboardMarkup(btns) if len(btns) != 0 else None
             )
@@ -74,40 +80,113 @@ async def start_msg(client, message):
             await editMessage(temp, "<b>File Not Found !</b>")
     else:
         await editMessage(temp, "<b>Input Link is Invalid for Usage !</b>")
-    
+
+
 @bot.on_message(command('pause') & private & user(Var.ADMINS))
 async def pause_fetch(client, message):
     ani_cache['fetch_animes'] = False
     await sendMessage(message, "`Successfully Paused Fetching Animes...`")
 
+
 @bot.on_message(command('resume') & private & user(Var.ADMINS))
-async def pause_fetch(client, message):
+async def resume_fetch(client, message):
     ani_cache['fetch_animes'] = True
     await sendMessage(message, "`Successfully Resumed Fetching Animes...`")
+
 
 @bot.on_message(command('log') & private & user(Var.ADMINS))
 @new_task
 async def _log(client, message):
     await message.reply_document("log.txt", quote=True)
 
+
 @bot.on_message(command('addlink') & private & user(Var.ADMINS))
 @new_task
-async def add_task(client, message):
-    if len(args := message.text.split()) <= 1:
+async def add_link(client, message):
+    args = message.text.split()
+    if len(args) <= 1:
         return await sendMessage(message, "<b>No Link Found to Add</b>")
-    
-    Var.RSS_ITEMS.append(args[0])
-    req_msg = await sendMessage(message, f"`Global Link Added Successfully!`\n\n    • **All Link(s) :** {', '.join(Var.RSS_ITEMS)[:-2]}")
+    Var.RSS_ITEMS.append(args[1])
+    await sendMessage(message, f"`Global Link Added Successfully!`\n\n    • **All Link(s) :** {', '.join(Var.RSS_ITEMS)}")
+
 
 @bot.on_message(command('addtask') & private & user(Var.ADMINS))
 @new_task
 async def add_task(client, message):
-    if len(args := message.text.split()) <= 1:
+    args = message.text.split()
+    if len(args) <= 1:
         return await sendMessage(message, "<b>No Task Found to Add</b>")
-    
     index = int(args[2]) if len(args) > 2 and args[2].isdigit() else 0
     if not (taskInfo := await getfeed(args[1], index)):
         return await sendMessage(message, "<b>No Task Found to Add for the Provided Link</b>")
-    
-    ani_task = bot_loop.create_task(get_animes(taskInfo.title, taskInfo.link, True))
+    bot_loop.create_task(get_animes(taskInfo.title, taskInfo.link, True))
     await sendMessage(message, f"<i><b>Task Added Successfully!</b></i>\n\n    • <b>Task Name :</b> {taskInfo.title}\n    • <b>Task Link :</b> {args[1]}")
+
+
+@bot.on_message(command('addmagnet') & private & user(Var.ADMINS))
+@new_task
+async def add_magnet(client, message):
+    args = message.text.split(maxsplit=1)
+    if len(args) <= 1 or not args[1].startswith("magnet:"):
+        return await sendMessage(message, "<b>Please provide a valid magnet link!</b>\n\n<i>Usage: /addmagnet magnet:?xt=...</i>")
+
+    magnet = args[1].strip()
+
+    try:
+        parsed = urlparse(magnet)
+        dn = parse_qs(parsed.query).get('dn', [''])[0]
+        name = unquote(dn).strip()
+    except Exception:
+        name = ""
+
+    if not name:
+        return await sendMessage(message, "<b>Could not extract anime name from magnet link!</b>")
+
+    await sendMessage(
+        message,
+        f"<i><b>Magnet Task Added!</b></i>\n\n"
+        f"    • <b>Anime Name :</b> <code>{name}</code>\n"
+        f"    • <b>Processing...</b>"
+    )
+    bot_loop.create_task(get_animes(name, magnet, True))
+
+
+@bot.on_message(command('addtorrent') & private & user(Var.ADMINS))
+async def add_torrent_cmd(client, message):
+    uid = message.from_user.id
+    pending_torrent[uid] = True
+    await sendMessage(message, "<b>Send your .torrent file now:</b>")
+
+
+@bot.on_message(document & private & user(Var.ADMINS))
+@new_task
+async def handle_torrent_file(client, message):
+    uid = message.from_user.id
+    if not pending_torrent.get(uid):
+        return
+    pending_torrent.pop(uid, None)
+
+    doc = message.document
+    if not doc.file_name.endswith(".torrent"):
+        return await sendMessage(message, "<b>Invalid file! Please send a .torrent file.</b>")
+
+    temp = await sendMessage(message, "<i>Downloading torrent file...</i>")
+    torrent_path = f"torrents/{doc.file_name}"
+
+    try:
+        await client.download_media(message, file_name=torrent_path)
+    except Exception as e:
+        await rep.report(str(e), "error")
+        return await editMessage(temp, "<b>Failed to download torrent file!</b>")
+
+    name = await TorDownloader.get_name_from_torfile(torrent_path)
+    if not name:
+        return await editMessage(temp, "<b>Could not extract anime name from torrent file!</b>")
+
+    await editMessage(
+        temp,
+        f"<i><b>Torrent Task Added!</b></i>\n\n"
+        f"    • <b>Anime Name :</b> <code>{name}</code>\n"
+        f"    • <b>Processing...</b>"
+    )
+    bot_loop.create_task(get_animes(name, torrent_path, True))
